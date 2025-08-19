d56918b79f285df36a8a0c000c1895c583aa2e8e
Bikramjit Chowdhury <bkrrulez@gmail.com>
2025-08-19 12:43:20 +0000

    still the logo is different in the login page. it is pathetic

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,19 +8,19 @@ export function Logo({ className, ...props }: React.SVGProps<SVGSVGElement> & { 
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="none"
+            stroke="currentColor"
             strokeWidth="1.5"
             strokeLinecap="round"
             strokeLinejoin="round"
             className="h-8 w-8"
             {...props}
             >
-            <path stroke="hsl(var(--primary))" d="M15.5 2.5h-8A2 2 0 0 0 5.5 4.5v15a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-15a2 2 0 0 0-2-2z" />
-            <path stroke="hsl(var(--primary))" d="M13.5 6.5a2 2 0 1 1-4 0 2 2 0 0 1 4 0z" />
-            <path stroke="hsl(var(--accent-foreground))" d="M12.5 2.5h-2a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-1a1 1 0 0 0-1-1z" />
-            <path stroke="hsl(var(--primary))" d="M16.5 12.5h-10" />
-            <path stroke="hsl(var(--accent-foreground))" d="M16.5 15.5h-10" />
-            <path stroke="hsl(var(--primary))" d="M14.5 10.5c0 1.66-1.12 3-2.5 3s-2.5-1.34-2.5-3" />
-
+            <path d="M15.5 2.5h-8A2 2 0 0 0 5.5 4.5v15a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-15a2 2 0 0 0-2-2z" />
+            <path d="M13.5 6.5a2 2 0 1 1-4 0 2 2 0 0 1 4 0z" />
+            <path d="M12.5 2.5h-2a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-1a1 1 0 0 0-1-1z" />
+            <path d="M16.5 12.5h-10" />
+            <path d="M16.5 15.5h-10" />
+            <path d="M14.5 10.5c0 1.66-1.12 3-2.5 3s-2.5-1.34-2.5-3" />
         </svg>
       <span className="text-2xl font-bold tracking-tight text-primary">
         contac<span className="text-accent-foreground">TU</span>
