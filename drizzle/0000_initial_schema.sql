f601551698877156e2d4986f45f91cd9671f56c4
Bikramjit Chowdhury <bkrrulez@gmail.com>
2025-08-20 14:12:36 +0000

    check the log.
    is it fine now?
    
    npm run db:migrate
    
    > nextn@0.1.0 db:mi

--- drizzle/0000_initial_schema.sql
+++ drizzle/0000_initial_schema.sql
@@ -1,3 +1,22 @@
+CREATE TABLE IF NOT EXISTS "contacts" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"first_name" varchar(256) NOT NULL,
+	"last_name" varchar(256) NOT NULL,
+	"address" text,
+	"birthday" date,
+	"notes" text,
+	"avatar" varchar(256)
+);
+
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(256) NOT NULL,
+	"email" varchar(256) NOT NULL,
+	"role" text NOT NULL,
+	"avatar" varchar(256),
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+
 CREATE TABLE IF NOT EXISTS "contact_associated_names" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"contact_id" integer NOT NULL,
@@ -19,12 +38,6 @@ CREATE TABLE IF NOT EXISTS "contact_organizations" (
 	"department" varchar(256)
 );
 
-DO $$ BEGIN
- CREATE TYPE "public"."phone_type" AS ENUM('Telephone', 'Mobile');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
-
 CREATE TABLE IF NOT EXISTS "contact_phones" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"contact_id" integer NOT NULL,
@@ -42,25 +55,6 @@ CREATE TABLE IF NOT EXISTS "contact_urls" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"contact_id" integer NOT NULL,
 	"url" varchar(256) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS "contacts" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"first_name" varchar(256) NOT NULL,
-	"last_name" varchar(256) NOT NULL,
-	"address" text,
-	"birthday" date,
-	"notes" text,
-	"avatar" varchar(256)
-);
-
-CREATE TABLE IF NOT EXISTS "users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" varchar(256) NOT NULL,
-	"email" varchar(256) NOT NULL,
-	"role" text NOT NULL,
-	"avatar" varchar(256),
-	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
 
 DO $$ BEGIN
