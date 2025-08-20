746cf03d002cc17a5df314e6543dc9c98392841e
Bikramjit Chowdhury <bkrrulez@gmail.com>
2025-08-20 14:11:18 +0000

    studio-232707639:~/studio/drizzle{main}$ npm run db:migrate
    > nextn@0.1

--- drizzle/0000_initial_schema.sql
+++ drizzle/0000_initial_schema.sql
@@ -1,21 +1,15 @@
-DO $$ BEGIN
- CREATE TYPE "public"."phone_type" AS ENUM('Telephone', 'Mobile');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "contact_associated_names" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"contact_id" integer NOT NULL,
 	"name" varchar(256) NOT NULL
 );
---> statement-breakpoint
+
 CREATE TABLE IF NOT EXISTS "contact_emails" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"contact_id" integer NOT NULL,
 	"email" varchar(256) NOT NULL
 );
---> statement-breakpoint
+
 CREATE TABLE IF NOT EXISTS "contact_organizations" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"contact_id" integer NOT NULL,
@@ -24,26 +18,32 @@ CREATE TABLE IF NOT EXISTS "contact_organizations" (
 	"team" varchar(256) NOT NULL,
 	"department" varchar(256)
 );
---> statement-breakpoint
+
+DO $$ BEGIN
+ CREATE TYPE "public"."phone_type" AS ENUM('Telephone', 'Mobile');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
 CREATE TABLE IF NOT EXISTS "contact_phones" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"contact_id" integer NOT NULL,
 	"phone" varchar(50) NOT NULL,
 	"type" "phone_type" NOT NULL
 );
---> statement-breakpoint
+
 CREATE TABLE IF NOT EXISTS "contact_social_links" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"contact_id" integer NOT NULL,
 	"link" varchar(256) NOT NULL
 );
---> statement-breakpoint
+
 CREATE TABLE IF NOT EXISTS "contact_urls" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"contact_id" integer NOT NULL,
 	"url" varchar(256) NOT NULL
 );
---> statement-breakpoint
+
 CREATE TABLE IF NOT EXISTS "contacts" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"first_name" varchar(256) NOT NULL,
@@ -53,7 +53,7 @@ CREATE TABLE IF NOT EXISTS "contacts" (
 	"notes" text,
 	"avatar" varchar(256)
 );
---> statement-breakpoint
+
 CREATE TABLE IF NOT EXISTS "users" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"name" varchar(256) NOT NULL,
@@ -62,37 +62,37 @@ CREATE TABLE IF NOT EXISTS "users" (
 	"avatar" varchar(256),
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
---> statement-breakpoint
+
 DO $$ BEGIN
  ALTER TABLE "contact_associated_names" ADD CONSTRAINT "contact_associated_names_contact_id_contacts_id_fk" FOREIGN KEY ("contact_id") REFERENCES "public"."contacts"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
---> statement-breakpoint
+
 DO $$ BEGIN
  ALTER TABLE "contact_emails" ADD CONSTRAINT "contact_emails_contact_id_contacts_id_fk" FOREIGN KEY ("contact_id") REFERENCES "public"."contacts"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
---> statement-breakpoint
+
 DO $$ BEGIN
  ALTER TABLE "contact_organizations" ADD CONSTRAINT "contact_organizations_contact_id_contacts_id_fk" FOREIGN KEY ("contact_id") REFERENCES "public"."contacts"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
---> statement-breakpoint
+
 DO $$ BEGIN
  ALTER TABLE "contact_phones" ADD CONSTRAINT "contact_phones_contact_id_contacts_id_fk" FOREIGN KEY ("contact_id") REFERENCES "public"."contacts"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
---> statement-breakpoint
+
 DO $$ BEGIN
  ALTER TABLE "contact_social_links" ADD CONSTRAINT "contact_social_links_contact_id_contacts_id_fk" FOREIGN KEY ("contact_id") REFERENCES "public"."contacts"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
---> statement-breakpoint
+
 DO $$ BEGIN
  ALTER TABLE "contact_urls" ADD CONSTRAINT "contact_urls_contact_id_contacts_id_fk" FOREIGN KEY ("contact_id") REFERENCES "public"."contacts"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
