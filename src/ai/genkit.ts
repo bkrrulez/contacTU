342bd23e28a8c0c7008b0ed3dd9d38e14a98a12a
Bikramjit Chowdhury <bkrrulez@gmail.com>
2025-09-02 13:58:52 +0000

    GEMINI_API_KEY=AIzaSyBvo9GEsd5K1chqG7BvvZ5-QT2vIw9xTt4
    has been added t

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,9 +4,7 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      // You can pass your Gemini API key here,
-      // or set the GEMINI_API_KEY environment variable.
-      // apiKey: process.env.GEMINI_API_KEY,
+      apiKey: process.env.GEMINI_API_KEY,
     }),
   ],
   logLevel: 'debug',
