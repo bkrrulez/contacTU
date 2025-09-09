a27fcaec8e1c441bb511a3209b86072a15f6c7da
Bikramjit Chowdhury <bkrrulez@gmail.com>
2025-09-09 12:39:53 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false,
+  devIndicators: {
+    buildActivity: false,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
